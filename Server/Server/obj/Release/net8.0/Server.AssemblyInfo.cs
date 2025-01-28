99d81abed028d271283a67d0dfbabd50c378f2cd
Alex <ax90790@gmail.com>
2025-01-28 23:03:21 +0500

    s1

--- Server/Server/obj/Release/net8.0/Server.AssemblyInfo.cs
+++ Server/Server/obj/Release/net8.0/Server.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eba8599e170113ccca4d8a7f5a5d9ad46faa8bcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
